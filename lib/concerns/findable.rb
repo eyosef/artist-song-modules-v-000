35ff7e9713f6819c771757779b19a61bab95680d
Eden Mesfun <emesfun@gmail.com>
2018-02-16 02:01:56 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,19 @@
 module Findable
 
   module ClassMethods
-  
+
     def find_by_name
+
+      def self.find_by_name(name)
+        @@artists.detect{|a| a.name == name}
+      end
+
+      def self.find_by_name(name)
+        @@songs.detect{|a| a.name == name}
+      end
+      
     end
 
-  end 
+  end
 
 end
