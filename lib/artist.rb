2195624eac671867027463d0f1a768c29e21e0d2
Eden Mesfun <emesfun@gmail.com>
2018-02-16 02:04:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,10 +7,10 @@ class Artist
   @@artists = []
 
   extend Memorable::ClassMethods
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # 
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
     @@artists << self
