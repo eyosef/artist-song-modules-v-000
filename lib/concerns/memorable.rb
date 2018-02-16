7cb11ff70bb72b781d9b56c9f96fecb4ff0acab8
Eden Mesfun <emesfun@gmail.com>
2018-02-16 01:40:58 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,14 +2,22 @@
 
 module Memorable
 
-module ClassMethods
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+      def reset_all
+        self.all.clear
+      end
 
-  def count
-    @@artists.count
-  end
-end
+      def count
+        @@artists.count
+      end
+
+      def self.reset_all
+        @@songs.clear
+      end
+
+      def self.count
+        self.all.count
+      end
+    end
 
 end
